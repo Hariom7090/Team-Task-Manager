679e9a75486b750206c83d1cbf78d42f07d372e5
Hariom7090 <hariom7090.ca22@chitkara.edu.in>
2026-05-11 18:54:56 +0530

    fix: body parsing issue  register API

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import  PrismaClient  from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 import authRoutes from './routes/authRoutes.js';
 import projectRoutes from './routes/projectRoutes.js';
